c225ccde6660a4d90beb7e84914cef0b0598cb96
Dovgal <ipz244_dto@student.ztu.edu.ua>
2026-05-18 17:51:30 +0300

    feat: implement polymorphic Note models and NoteFactory

---
+++ NoteReminder/NoteReminder/Models/ReminderNote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NoteReminder.Models
+{
+    public class ReminderNote : Note
+    {
+        public DateTime ReminderTime { get; set; }
+        public bool IsTriggered { get; set; }
+
+        public override string GetNoteType() => "Нагадування";
+        public override string GetSummary() => "[Діє до: " + ReminderTime.ToString("dd.MM HH:mm") + "] " + Content;
+    }
+}

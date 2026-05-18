863c8ef044c6f1c415efa6c12794e689c6259f59
Dovgal <ipz244_dto@student.ztu.edu.ua>
2026-05-18 17:50:41 +0300

    chore: initial commit with project structure and gitignore

---
+++ NoteReminder/NoteReminder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NoteReminder
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Запускаємо нашу головну форму Form1
+            Application.Run(new Form1());
+        }
+    }
+}
